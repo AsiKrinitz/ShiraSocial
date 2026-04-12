2dd2be3b9c8b655bc5be1391d20457ba799de7d8
AsiKrinitz <AsiKrinitz@gmail.com>
2026-04-12 21:24:41 +0300

    add the basename route

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/ShiraSocial">
       <div className="app-container">
         <Navbar />
         <main className="content">
